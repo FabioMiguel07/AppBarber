223e5117e69bc3e8df36d250ff68a1a404d00e1a
fabio <fabio>
2020-02-01 14:46:53 -0300

    configuracao banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'appbarber',
+    define: {
+        timestamps: true,
+        underscore: true,
+        underscoreAll: true
+    }
+};
+
